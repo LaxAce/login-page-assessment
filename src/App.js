b0a32c43bdd217afbd893170707b9215753bd5ea
Abdulhafeez Hameed <laxhameedhafeez@gmail.com>
2022-01-29 14:24:23 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import img from "./img/clip-message-sent 1.png";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="login-container">
+        <h1>Digital</h1>
+        <h2>Artificial Intelligence Driving Results For The Travel Industry</h2>
+        <p>Welcome back! Please login to your account.</p>
+        <form>
+          <div className="text-wrapper email-wrapper">
+            <input
+              type="text"
+              name="email"
+              autoComplete="off"
+              autoCapitalize="off"
+              required
+            />
+            <label htmlFor="email">Email Address</label>
+          </div>
+          <div className="text-wrapper password-wrapper">
+            <input
+              type="password"
+              name="password"
+              autoComplete="off"
+              required
+            />
+            <label htmlFor="password">Password</label>
+          </div>
+          <div className="remember-forgot-wrapper">
+            <div className="remember-me">
+              <input type="checkbox" />
+              <span>Remember Me</span>
+            </div>
+            <p>
+              <a href="#">Forgot Password?</a>
+            </p>
+          </div>
+          <div className="btn-wrapper">
+            <button type="submit" className="btn btn-login">
+              Login
+            </button>
+            <a href="#" className="btn btn-signup">
+              Sign Up
+            </a>
+          </div>
+        </form>
+        <div className="options">
+          <p>Or login with</p>
+          <ul>
+            <li>
+              <a href="#">Facebook</a>
+            </li>
+            <li>
+              <a href="#">Linkedin</a>
+            </li>
+            <li>
+              <a href="#">Google</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="illustrator-container">
+        <img src={img} alt="message sent"></img>
+      </div>
     </div>
   );
 }
